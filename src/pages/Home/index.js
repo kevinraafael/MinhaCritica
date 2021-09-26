a242889f6c0b435697827711b556c6bbf5dd4104
Kevin Francelino <kevin.francelino@redetendencia.com.br>
2021-09-26 18:51:36 -0400

    ajustes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,4 @@
 function menuFilme() {
 	window.location.href = "..\\.\\Movie\\index.html"
 }
-function p1(){
-	var a =5;
-	var b =10;
-	if(a===5){
-		let a=4;
-		b=1;
-		console.log(a);
-		console.log(b);
 
-	}
-	console.log(a);
-	console.log(b);
-}
