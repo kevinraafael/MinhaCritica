c4abb72a799e57b31366b9177d2914daeab7a3d9
Guilherme Moreno Guimarães <gmorenoguimaraes@yahoo.com.br>
2021-11-05 00:32:50 -0400

    main

---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+function login() { 
+	window.location.href = "..\\.\\Login\\index.html"
+}
+//Redirecionamento por filmes
+function movieMenu() {
+	window.location.href = "..\\.\\Movie\\suicideSquad.html" //mudar
+}
+function goToSuicideSquad() {
+	window.location.href = "..\\.\\Movie\\suicideSquad.html"
+}
+function goToTheTallMan() {
+	window.location.href = "..\\.\\Movie\\theTallMan.html"
+}
+/*
+function perfil() {
+	window.location.href = "..\\.\\Profile\\index.html"
+}
+function registration() {
+	window.location.href = "..\\.\\registration\\index.html"
+}
+*/
+//TODO pegar o  valor do input e transferir prao outra pagina
+function search() {
+	var search = document.getElementById("search").value; 
+	window.location.href = ".\\search\\index.html"
+}
