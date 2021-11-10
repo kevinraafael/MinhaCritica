504cf0a4032b484c1a4d9e041dbfbfdce3f491eb
kevin.francelino <kevin.francelino@grupocard.com.br>
2021-11-10 13:23:34 -0400

    permitido a insercao de filmes no banco , exceto imagem

--- src/pages/AddMovies/index.php
+++ src/pages/AddMovies/index.php
@@ -9,17 +9,15 @@ try {
 
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (key_exists('nome', $_POST) && $_POST['nome'] !== '') {
-      $stm = $db->prepare('INSERT INTO Midia (nome) VALUES (:nome)');
+      $stm = $db->prepare('INSERT INTO Midia (nome,tipo,descricao,trailer) VALUES (:nome,:tipo,:descricao,:trailer)');
       // $stm->execute(array(':nome' => $_POST['nome'],),);
-     
-      
-      $stm->execute(
-        array('nome' => $_POST['nome'] )
-      );
 
-      
+
+      $stm->execute(
+        array('nome' => $_POST['nome'], 'tipo' => $_POST['tipo'],  'trailer' => $_POST['trailer'], 'descricao' => $_POST['descricao'])
+      );
     }
-  } 
+  }
 
   //$usuarios = $db->query('SELECT * FROM Usuarios ORDER BY adicionado_em DESC')->fetchAll();
 } catch (\Throwable $th) {
@@ -30,93 +28,75 @@ try {
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="index.css" />
-    <!--Link do Icone-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    rel="stylesheet">
-    <title>Minha Crtitica</title>
-  </head>
-  <body>
 
-    <body class="body">
-       <!-- MENU-->
-        <div class="header">
-          <div id="mainMenu">
-            <div class="searchBar">
-              <h1>Minha Crítica</h1>
-              <input
-                type="text"
-                id="search"
-                name="searchBox"
-                placeholder="Pesquise aqui"
-              />
-            </div>
-            <nav id="navigation">
-              <ul>
-                <li>
-                  <a> Início </a>
-                </li>
-                <li>
-                  <a onclick="menuFilme()"> Filmes </a>
-                </li>
-                <li>
-                  <a> Séries </a>
-                </li>
-                <li>
-                  <a> Livros </a>
-                </li>
-                <li>
-                  <a> Login </a>
-                </li>
-              </ul>
-            </nav>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="index.css" />
+  <!--Link do Icone-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <title>Minha Crtitica</title>
+</head>
+
+<body>
+
+  <body class="body">
+    <!-- MENU-->
+    <div class="header">
+      <div id="mainMenu">
+        <div class="searchBar">
+          <h1>Minha Crítica</h1>
+          <input type="text" id="search" name="searchBox" placeholder="Pesquise aqui" />
+        </div>
+        <nav id="navigation">
+          <ul>
+            <li>
+              <a> Início </a>
+            </li>
+            <li>
+              <a onclick="menuFilme()"> Filmes </a>
+            </li>
+            <li>
+              <a> Séries </a>
+            </li>
+            <li>
+              <a> Livros </a>
+            </li>
+            <li>
+              <a> Login </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <!-- MENU-->
+    <!-- Capa , Título e Sinopse-->
+    <form method="POST" class="formStyle">
+      <div class="divPrincipal">
+        <div class="">
+          <!-- Esta div e responsavel por pela capa, categoria, duração e URL do filme-->
+          <div class="capa_background">
+            <p>Arraste e solte capa aqui</p>
+            <i class="material-icons" id="arrow">north</i>
+          </div>
+          <div class="formMin">
+            <input name="nome" class="all" type="text" placeholder="Título">
+            <input name="trailer" class="all" type="text" placeholder="URL do Trailer">
           </div>
         </div>
-      <!-- MENU-->
-      <!-- Capa , Título e Sinopse-->
-      <div class="divPrincipal" >
-        <div class="divCapa">
-          <!-- Esta div e responsavel por pela capa, categoria, duração e URL do filme-->
-           <h1 class="tituloCapa"> Capa</h1>
-           <div class="capa_background">
-             <p>Arraste e solte capa aqui</p>
-              <i class="material-icons" id="arrow">north</i>
-             
-           </div> 
-         <div class="formCapa">
-           <form class="inputCapa">
-             <input type="text" placeholder="Categoria">
-            
-             <input type="text" placeholder="URL do Trailer">
-           </form>
-         </div>
-      </div>
-      <!-- Esta div e responsavel por pela capa, categoria, duração e URL do filme-->
-      <div class="divTituloandSinopse">
-        <div class="divTitulo">
-          <h1>Título</h1>
-          <form method = "POST" action="">
-             <input  name = "nome" class="inputTitulo"  type="text" placeholder="Insira o titulo da obra">
-             <button  type="submit" class="botao">adicionar</button>
-            </form>
+        <!-- Esta div e responsavel por pela capa, categoria, duração e URL do filme-->
+        <div class="capa">
+          <input class="all" name="tipo" type="text" placeholder="Categoria da obra">
+          <input class="all" name="descricao" type="text" placeholder="Insira a sinopse da obra">
+          <button type="submit" class="botao">adicionar</button>
         </div>
-        <div class="divSinopse">
-          <h1>Sinopse</h1>
-          <form action="">  
-            <input class="inputSinopse" type="text" placeholder="Insira a sinopse da obra">
-            
-          </form>
-        </div>  
-        
-      </div>  
-      
-      
-    </div>
-        <!-- Capa , Título e Sinopse-->
-    
+      </div>
+      </div>
+      </div>
+    </form>
+    <!-- Capa , Título e Sinopse-->
+
   </body>
+
 </html>
