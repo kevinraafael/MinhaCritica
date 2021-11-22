5b815ad5acbb5cefe9f7316f6f30adf899178b14
kevinraafael <kevinfrancelino@gmail.com>
2021-11-21 23:32:36 -0400

    alteracoes main

--- src/pages/AddMovies/index.php
+++ src/pages/AddMovies/index.php
@@ -33,7 +33,7 @@ try {
       mkdir($diretorio, 0755);  /// 0755 é o número da permissão 
       //var_dump($imagem);
       $imagem = $_FILES['imagem'];
-      $nomeArquivo = 'Hudson_' . $imagem['name']; // substitui pelo id da sessao
+      $nomeArquivo = 'Imagem' . $imagem['name']; // substitui pelo id da sessao
 
       $stm->execute(
         array('nome' => $_POST['nome'], 'tipo' => $_POST['tipo'],  'trailer' => $_POST['trailer'], 'descricao' => $_POST['descricao'], 'imagem' => $nomeArquivo)
@@ -66,67 +66,67 @@ try {
 
 
 
-  <body class="body">
-    <!-- MENU-->
-    <div class="header">
-      <div id="mainMenu">
-        <div class="searchBar">
-          <h1>Minha Crítica</h1>
-          <input type="text" id="search" name="searchBox" placeholder="Pesquise aqui" />
-        </div>
-        <input type="checkbox" id="bt_menu" />
-        <label for="bt_menu">&#9776;</label>
-        <nav id="navigation">
-          <ul>
-            <li>
-              <a> Início </a>
-            </li>
-            <li>
-              <a onclick="menuFilme()"> Filmes </a>
-            </li>
-            <li>
-              <a> Séries </a>
-            </li>
-            <li>
-              <a> Livros </a>
-            </li>
-            <li>
-              <a> Login </a>
-            </li>
-          </ul>
-        </nav>
+<body class="body">
+  <!-- MENU-->
+  <div class="header">
+    <div id="mainMenu">
+      <div class="searchBar">
+        <h1>Minha Crítica</h1>
+        <input type="text" id="search" name="searchBox" placeholder="Pesquise aqui" />
       </div>
+      <input type="checkbox" id="bt_menu" />
+      <label for="bt_menu">&#9776;</label>
+      <nav id="navigation">
+        <ul>
+          <li>
+            <a> Início </a>
+          </li>
+          <li>
+            <a onclick="menuFilme()"> Filmes </a>
+          </li>
+          <li>
+            <a> Séries </a>
+          </li>
+          <li>
+            <a> Livros </a>
+          </li>
+          <li>
+            <a> Login </a>
+          </li>
+        </ul>
+      </nav>
     </div>
-    <!-- MENU-->
-    <!-- Capa , Título e Sinopse-->
-    <form enctype="multipart/form-data" method="POST" class="formStyle">
-      <div class="divPrincipal">
-        <div>
-          <!-- Esta div e responsavel por pela capa, categoria, duração e URL do filme-->
-          <h1 class="capa">Capa</h1>
-          <div class="formMin">
-            <!--<input type="hidden" name="MAX_FILE_SIZE" value="30000" />-->
-            <br/>
-            <div class="inputCapa">
-            <input class="all" id="inserirImg" type="file" name="imagem"  />
-            </div>
-            <h1 class="capa">Titulo</h1>
-            <input name="nome" class="all" type="text" placeholder="Título" />
-            <input name="trailer" class="all" type="text" placeholder="URL do Trailer" />
-         </div>
-        </div>
+  </div>
+  <!-- MENU-->
+  <!-- Capa , Título e Sinopse-->
+  <form enctype="multipart/form-data" method="POST" class="formStyle">
+    <div class="divPrincipal">
+      <div>
         <!-- Esta div e responsavel por pela capa, categoria, duração e URL do filme-->
-        <div class="divCapa">
-          
-          <input class="all" name="tipo" type="text" placeholder="Categoria da obra">
-          <h1 class="capa">Sinopse</h1>
-          <input class="all" name="descricao" type="text" placeholder="Insira a sinopse da obra">
+        <h1 class="capa">Capa</h1>
+        <div class="formMin">
+          <!--<input type="hidden" name="MAX_FILE_SIZE" value="30000" />-->
+          <br />
+          <div class="inputCapa">
+            <input class="all" id="inserirImg" type="file" name="imagem" />
+          </div>
+          <h1 class="capa">Titulo</h1>
+          <input name="nome" class="all" type="text" placeholder="Título" />
+          <input name="trailer" class="all" type="text" placeholder="URL do Trailer" />
         </div>
-        <button name="sendCadImg" type="submit" class="botao">adicionar</button>
       </div>
-    </form>
-    <!-- Capa , Título e Sinopse-->
+      <!-- Esta div e responsavel por pela capa, categoria, duração e URL do filme-->
+      <div class="divCapa">
 
-  </body>
+        <input class="all" name="tipo" type="text" placeholder="Categoria da obra">
+        <h1 class="capa">Sinopse</h1>
+        <input class="all" name="descricao" type="text" placeholder="Insira a sinopse da obra">
+      </div>
+      <button name="sendCadImg" type="submit" class="botao">adicionar</button>
+    </div>
+  </form>
+  <!-- Capa , Título e Sinopse-->
+
+</body>
 
 </html>
